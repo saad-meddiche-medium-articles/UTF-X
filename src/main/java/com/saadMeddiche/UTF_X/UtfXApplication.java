0956664b2eb3f6b3689591687c7133ca7e9616db
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-07-27 13:00:54 +0100

    The beginning :)

---
+++ src/main/java/com/saadMeddiche/UTF_X/UtfXApplication.java
@@ -0,0 +1,13 @@
+package com.saadMeddiche.UTF_X;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtfXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtfXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadMeddiche/UTF_X/UtfXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadMeddiche.UTF_X;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtfXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
