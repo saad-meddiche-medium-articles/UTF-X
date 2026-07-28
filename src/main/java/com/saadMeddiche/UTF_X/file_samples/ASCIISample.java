9bb6f79ff079d334174e0406b75abd6393bdaf0c
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-07-28 09:29:14 +0100

    feat: add ascii sample

---
+++ src/main/java/com/saadMeddiche/UTF_X/file_samples/ASCIISample.java
@@ -0,0 +1,27 @@
+package com.saadMeddiche.UTF_X.file_samples;
+
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+@Component
+public class ASCIISample implements FileSample {
+
+    @Override
+    public String getFileName() {
+        return "ascii.txt";
+    }
+
+    @Override
+    public Path getFilePath() {
+        return Path.of(getFileName());
+    }
+
+    @Override
+    public Charset getStandardCharset() {
+        return StandardCharsets.US_ASCII;
+    }
+
+}
